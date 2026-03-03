902db61b050642cffb8db45e488246bb655e5be4
Matej Smutný <booking.vividhues@gmail.com>
2026-03-03 20:52:27 +0100

    layout fix + next level design

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+/* smooth scroll */
 document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
 anchor.addEventListener("click",function(e){
 e.preventDefault();
 document.querySelector(this.getAttribute("href"))
 .scrollIntoView({behavior:"smooth"});
 });
+});
+
+/* header blur on scroll */
+
+const header = document.getElementById("header");
+
+window.addEventListener("scroll",()=>{
+if(window.scrollY>40){
+header.style.background="rgba(0,0,0,.75)";
+}else{
+header.style.background="rgba(0,0,0,.45)";
+}
 });
