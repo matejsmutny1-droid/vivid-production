c2e9e0fa178a56e4bcd045ff249a9d64fadfedba
Matej Smutný <booking.vividhues@gmail.com>
2026-03-03 19:25:15 +0100

    mobile fix + animations

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-function sayHello() {
-  alert("Kontaktujte Vivid Production s.r.o.");
-}
+/* MOBILE MENU */
+
+const toggle = document.getElementById("menuToggle");
+const nav = document.getElementById("navMenu");
+
+toggle.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
+
+/* SCROLL REVEAL */
+
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+  reveals.forEach(el => {
+    const top = el.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      el.classList.add("active");
+    }
+  });
+});
