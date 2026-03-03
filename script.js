1c82a2df2cbc650b7da552c5d827de31810a1384
Matej Smutný <booking.vividhues@gmail.com>
2026-03-03 19:31:16 +0100

    final mobile redesign

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
-/* MOBILE MENU */
+const toggle=document.getElementById("menuToggle");
+const nav=document.getElementById("navMenu");
 
-const toggle = document.getElementById("menuToggle");
-const nav = document.getElementById("navMenu");
-
-toggle.addEventListener("click", () => {
-  nav.classList.toggle("active");
+toggle.addEventListener("click",()=>{
+nav.classList.toggle("active");
 });
 
-/* SCROLL REVEAL */
+const reveals=document.querySelectorAll(".reveal");
 
-const reveals = document.querySelectorAll(".reveal");
-
-window.addEventListener("scroll", () => {
-  reveals.forEach(el => {
-    const top = el.getBoundingClientRect().top;
-    if(top < window.innerHeight - 100){
-      el.classList.add("active");
-    }
-  });
+window.addEventListener("scroll",()=>{
+reveals.forEach(el=>{
+const top=el.getBoundingClientRect().top;
+if(top<window.innerHeight-100){
+el.classList.add("active");
+}
+});
 });
