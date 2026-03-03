6d3c4fd2cf55d2c0bca68bf402ee41bb8d3c6bb3
Matej Smutný <booking.vividhues@gmail.com>
2026-03-03 19:34:44 +0100

    final polish

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ el.classList.add("active");
 }
 });
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+anchor.addEventListener("click",function(e){
+e.preventDefault();
+document.querySelector(this.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+});
+});
+});
